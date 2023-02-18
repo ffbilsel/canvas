130bd1e490e68025edb56b9becd9edd7e214d202
fatih <furkan.bilsel@id3.com.tr>
2023-02-18 20:10:02 +0300

    last version with canvas library

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {useEffect, useRef, useState} from "react";
 import Calculator from "./calc/Calculator";
 
 // TODO responsive
+// TODO pause
+// TODO record
 
 function App() {
     let canvas = useRef(null);
@@ -19,8 +21,10 @@ function App() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     let mediaRecorder = useRef(null)
     let videoSource = useRef("")
+    const [coordinates, setCoordinates] = useState([0,0]);
 
     useEffect(() => {
+
         let interval;
         if (running) {
             interval = setInterval(() => {
@@ -41,6 +45,19 @@ function App() {
         });
     }, []);
 
+    useEffect(() => {
+        let grid = canvas.current.canvas.interface
+        grid.addEventListener('mousedown', event => {
+            let rect = grid.getBoundingClientRect()
+            setCoordinates([event.clientX - rect.left, event.clientY - rect.top])
+        })
+        let ctx = canvas.current.canvas.temp.getContext("2d")
+        ctx.fillStyle = brushColor;
+        ctx.font = "30px Arial";
+        ctx.fillStyle="rgb(255,255,255)";
+        ctx.fillRect(0, 0, 2000, 2000);
+    }, [])
+
     let inputFileRef = useRef(null);
     const [inputFile, setInputFile] = useState(null)
     const canvasWidth = window.innerWidth * 0.7;
@@ -58,23 +75,9 @@ function App() {
 
     async function createStream() {
 
-        return await navigator.mediaDevices.getDisplayMedia({
-            video: {
-                mediaSource: 'screen',
-            },
-            audio: false,
-        })
-            .then(async (e) => {
-
-                // For recording the mic audio
-                let audio = await navigator.mediaDevices.getUserMedia({
-                    audio: true, video: false })
-
-
-                return new MediaStream(
-                    [...e.getTracks(), ...audio.getTracks()]);
-            })
-
+        return await navigator.mediaDevices.getUserMedia({
+            audio: true, video: false }).then(async (e) =>
+            new MediaStream([ ...canvas.current.canvas.temp.captureStream(60).getTracks(), ...e.getTracks()]))
     }
 
     function startRecording(combined) {
@@ -137,11 +140,11 @@ function App() {
             <Col className={"center"}>
                 <Modal open={isModalOpen} onOk={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)} width={850}
                        footer={[<Button type={"primary"} onClick={() => setIsModalOpen(false)}>OK</Button>]} >
-                    <Row justify={"center"} align={"middle"}>
+                    <Row justify={"center"} align={"middle"} >
                         <video src={videoSource.current} controls style={{width: "720px", height: "100%"}}></video>
                     </Row>
                 </Modal>
-                <Row className={"up"} style={{marginBottom: "2%"}}>
+                <Row className={"up"} id={"canvasRow"} style={{marginBottom: "2%"}}>
                     <CanvasDraw ref={canvasDraw => (canvas.current = canvasDraw)} brushRadius={brushSize} brushColor={brushColor}
                                 canvasWidth={canvasWidth} canvasHeight={canvasHeight} imgSrc={inputFile != null ? URL.createObjectURL(inputFile) : ""}
                     enablePanAndZoom={panAndZoom}/>
@@ -174,7 +177,7 @@ function App() {
                 </Row>
             </Col>
             <Col className={"rightPane"} style={{marginLeft: "6%", width: "6%"}}>
-                <Row>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-solid fa-pen" onClick={() => {
                         setPen(true)
                         setBrushSize(2)
@@ -189,24 +192,29 @@ function App() {
                     }}></i>
                     <i className="fa-solid fa-eraser" onClick={() => canvas.current.undo()}></i>
                 </Row>
-                <Row>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-solid fa-trash" onClick={() => canvas.current.eraseAll()}></i>
                     <i className="fa-solid fa-rotate-right"  onClick={() => canvas.current.undo()}></i>
                     <i className="fa-solid fa-arrow-pointer" onClick={() => setPanAndZoom(true)}></i>
                 </Row>
-                <Row>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-solid fa-copy"></i>
                     <i className="fa-solid fa-image" onClick={() => inputFileRef.current.click()}><input type='file' ref={inputFileRef} onChange={(e) => setInputFile(e.target.files[0])} style={{display: 'none'}}/></i>
-                    <i className="fa-solid fa-a"></i>
+                    <i className="fa-solid fa-a" onClick={() => {
+                        console.log(coordinates)
+                        canvas.current.canvas.grid.getContext("2d").fillText("Hello World", coordinates[0], coordinates[1]);
+                    }
+                    }></i>
 
                 </Row>
-                <Row>
+                <Row align={"middle"} justify={"center"}>
                     <Popover content={<Calculator/>} placement={"leftBottom"} trigger="click">
                         <i className="fa-solid fa-calculator" ></i>
                     </Popover>
                     <i className="fa-solid fa-circle-info"></i>
                 </Row>
-                <Row>
+                <hr/>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-regular fa-square" style={{backgroundColor: "black", borderRadius: "3px"}} onClick={() => {
                         setBrushColor(setColor("#000000"))
                         setOriginalColor("#000000")
@@ -220,7 +228,7 @@ function App() {
                         setOriginalColor("#00FF00")
                     }}></i>
                 </Row>
-                <Row>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-regular fa-square" style={{backgroundColor: "yellow", borderRadius: "3px"}} onClick={() => {
                         setBrushColor(setColor("#FFFF00"))
                         setOriginalColor("#FFFF00")
@@ -234,15 +242,39 @@ function App() {
                         setOriginalColor("#FF0000")
                     }}></i>
                 </Row>
-                <Row align={"middle"}>
+                <hr/>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-regular fa-circle" style={{fontSize: "8px"}} onClick={()=> setBrushSize(isPen ? 1 : 3)}></i>
                     <i className="fa-regular fa-circle" style={{fontSize: "12px"}} onClick={()=> setBrushSize(isPen ? 1.5 : 5)}></i>
                     <i className="fa-regular fa-circle" style={{fontSize: "16px"}} onClick={()=> setBrushSize(isPen ? 2 : 7)}></i>
                 </Row>
-                <Row align={"middle"}>
+                <Row align={"middle"} justify={"center"}>
                     <i className="fa-regular fa-circle" style={{fontSize: "20px"}} onClick={()=> setBrushSize(isPen ? 2.3 : 10)}></i>
                     <i className="fa-regular fa-circle" style={{fontSize: "24px"}} onClick={()=> setBrushSize(isPen ? 2.6 : 13)}></i>
                     <i className="fa-regular fa-circle" style={{fontSize: "28px"}} onClick={()=> setBrushSize(isPen ? 3 : 15)}></i>
+                </Row>
+                <hr/>
+                <Row align={"middle"} justify={"center"}>
+                    <i className="fa-solid fa-arrows-left-right"></i>
+                    <i className="fa-solid fa-arrows-up-down"></i>
+                    <i className="fa-solid fa-square-arrow-up-right"></i>
+                </Row>
+                <Row align={"middle"} justify={"center"}>
+                    <i className="fa-solid fa-caret-up"></i>
+                    <i className="fa-solid fa-square"></i>
+                    <i className="fa-solid fa-rectangle-xmark"></i>
+                </Row>
+                <Row align={"middle"} justify={"center"}>
+                    <i className="fa-solid fa-draw-polygon"></i>
+                </Row>
+                <hr/>
+                <Row align={"middle"} justify={"center"}>
+                    <i className="fa-solid fa-plus"></i>
+                    <i className="fa-solid fa-arrow-right"></i>
+                    <i className="fa-solid fa-code-branch"></i>
+                </Row>
+                <Row align={"middle"} justify={"center"}>
+                    <i className="fa-solid fa-crop"></i>
                 </Row>
             </Col>
 
